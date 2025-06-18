ee0df6c4c741f20987fdb65025343e192725ebde
Letícia Leal - GrowAcademy <leticia.leal@growdev.academy>
2025-06-18 20:16:36 -0300

    refactor(routes): change HTTP methods and add validation for user routes
    - Switch from POST to GET for user endpoints as they are read operations
    - Add UUID validation for user ID parameter using express-validator

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,20 @@
 import express from "express";
+import { param } from "express-validator";
 
 import { UsersController } from "../controllers";
+import { dataValidation } from "../middlewares";
 
 export class UsersRoutes {
   public static bind() {
     const router = express.Router();
     const controller = new UsersController();
 
-    router.post("/users", controller.index);
-    router.post("/users", controller.getById);
+    router.get("/users", controller.index);
+    router.get(
+      "/users/:userId",
+      dataValidation([param("userId").isUUID()]),
+      controller.getById,
+    );
 
     return router;
   }
