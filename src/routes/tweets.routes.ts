dc56a5da6e13ae67a15647d29f71cee027905c32
Letícia Leal - GrowAcademy <leticia.leal@growdev.academy>
2025-07-23 10:01:39 -0300

    feat(tweets): add TweetsRoutes for tweet management including creation, replies, and user feeds

---
+++ src/routes/tweets.routes.ts
@@ -0,0 +1,64 @@
+import express from "express";
+import { body, param } from "express-validator";
+
+import { TweetsController } from "../controllers";
+import { authMiddleware, dataValidation } from "../middlewares";
+
+export class TweetsRoutes {
+  public static bind() {
+    const router = express.Router();
+    const controller = new TweetsController();
+
+    router.post(
+      "/tweets",
+      authMiddleware,
+      dataValidation([body("content").isString().isLength({ min: 1 })]),
+      controller.createTweet,
+    );
+
+    router.post(
+      "/replies",
+      authMiddleware,
+      dataValidation([
+        body("content").isString().isLength({ min: 1 }),
+        body("replyTo").isString().isUUID(),
+      ]),
+      controller.createReply,
+    );
+
+    router.get(
+      "/tweets/:id",
+      authMiddleware,
+      dataValidation([param("id").isUUID()]),
+      controller.findTweet,
+    );
+
+    router.put(
+      "/tweets/:id",
+      authMiddleware,
+      dataValidation([
+        param("id").isUUID(),
+        body("content").isString().isLength({ min: 1 }),
+      ]),
+      controller.updateTweet,
+    );
+
+    router.delete(
+      "/tweets/:id",
+      authMiddleware,
+      dataValidation([param("id").isUUID()]),
+      controller.deleteTweet,
+    );
+
+    router.get(
+      "/users/:userId/tweets",
+      authMiddleware,
+      dataValidation([param("userId").isUUID()]),
+      controller.listTweetsByUserId,
+    );
+
+    router.get("/tweets/feed", authMiddleware, controller.feed);
+
+    return router;
+  }
+}
